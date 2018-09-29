58eeb124f874d10736f3945a9dc3903154dbf2fd
Michael Tran <michael.tran@walmartlabs.com>
2018-09-28 18:33:01 -0700

    Matrix Work

--- Linear_Combinations.py
+++ Linear_Combinations.py
@@ -13,9 +13,9 @@
 # Vectors is the set of all possible linear combinations of those vectors
 
 # Quiz
-v_1 = [1 2 3]
-v_2 = [2 2 2]
-v_3 = [8 8 8]
+# v_1 = [1 2 3]
+# v_2 = [2 2 2]
+# v_3 = [8 8 8]
 
 # one vector can be defined as a lienar combination of the other
 # v_2 = 0.25 (v_3)
@@ -28,3 +28,46 @@ v_3 = [8 8 8]
 
 # When each vector in a set of vectors, vector can not be defined as a linear combination of the other vectors they are
 # a set of linearly independent vectors
+
+# Simplified set of equations
+# x = [-14 2]
+# y = [5 -1]
+#
+# z = [-13 3]
+
+# -14a + 5b = -13
+# 2a - b = 3
+
+# Two unknowns can be solved by:
+# Graphical method
+# Substituion method
+# Elimination method
+
+# Graphing solution
+# Draw both lines (linear plots) and find the intersection
+# The intersection is the solution to (a,b)
+
+# Substitution
+# Isolate a variable from one of the equations and substitue it in the scond
+# Method will reduce our system of equations ot a single equation with a single unknown
+
+# -14a + 5b = -13
+# 2a - b = 3 ==> b = 2a -3
+
+# -14a + 5(2a -3) = -13
+b = -4
+
+# ELIMINATION
+# Elimnate one of the variable by enforcing the same absolute  value to one of the scalars( coefficients)Michael Tran - m0t010b
+# -14a + 5b = -13
+#   2a -  b =   3
+
+# -14a + 5b = -13
+#  10a - 5b =   15
+
+# 4a = 2 => a = 1/2
+b = -4
+
+# Will a set of two linear equations with two unknowns always have a solution? And if so, will it always be a single solution?
+
+# A: There are infinite number of solutions
